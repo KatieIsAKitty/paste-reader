07c90843821e0de2479bc2f0687d66c422c7723c
Weir <kayne.weir@dxc.com>
2019-02-05 10:40:08 +1100

    v1.0! It works!

--- index.js
+++ index.js
@@ -2,21 +2,17 @@
 
 'use strict';
 
-const http = require('http');
-const { inspect } = require('util');
+const { createServer, get } = require('http');
 const { readFile, readdir } = require('fs').promises;
 
 const indexPaths = ['/', '/index.js', '', null, 'null', undefined];
 
-
-async function handleConnect(req, res) {
+const server = createServer(async (req, res) => {
   console.log(req.url);
   const stylePaths = await readdir('./styles/');
   for (let i = 0; i < stylePaths.length; i += 1) {
-    console.log(stylePaths[i]);
     stylePaths[i] = `/styles/${stylePaths[i]}`;
   }
-  console.log(stylePaths);
   if (indexPaths.includes(req.url)) {
     const html = await readFile('./templates/main.html');
     res.setHeader('Content-Type', 'text/html');
@@ -29,10 +25,23 @@ async function handleConnect(req, res) {
     res.end(css);
     return;
   }
+  if (req.url.startsWith('/https://pastebin.com/') || req.url.startsWith('/pastebin.com/')) {
+    const url = req.url.replace(/\/(https:\/\/)?pastebin.com\//i, '');
+    get(`http://pastebin.com/raw/${url}`, async (chunks) => {
+      let paste = `<html><head><link rel="stylesheet" type="text/css" href="http://${server.address().address}:${server.address().port}/styles/default.css"></head><body>`;
+      for await (const chunk of chunks) {
+        paste += chunk;
+      }
+      paste += '</body></html>';
+      paste = paste.replace(/[\n\r]+/g, '<br />');
+      res.setHeader('Content-Type', 'text/html');
+      res.end(paste);
+    });
+    return;
+  }
   res.setHeader('Content-Type', 'application/json');
-  res.end(inspect({ headers: res.getHeaders() }));
-}
+  res.end('you did a bad, try again');
+});
 
-const server = http.createServer(handleConnect);
 server.listen({ port: 8080, host: '127.0.0.1' });
 server.on('listening', () => console.log(server.address()));
